daad835daa027bfcd970d184ae677f26b4c17a0d
danishrahmankpm <140050542+danishrahmankpm@users.noreply.github.com>
2025-07-19 16:51:29 +0530

    inital setup

---
+++ auth-test-jwt/src/main/java/com/example/auth_test_jwt/AuthTestJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_test_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTestJwtApplication.class, args);
+	}
+
+}
---
+++ auth-test-jwt/src/test/java/com/example/auth_test_jwt/AuthTestJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_test_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTestJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
